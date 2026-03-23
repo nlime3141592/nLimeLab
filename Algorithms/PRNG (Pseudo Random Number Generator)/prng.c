068a6cbc45a8bd0861913a7e427a8c6d9a0fb113
YellowGreenLime <nlime3141592@naver.com>
2026-03-24 04:24:04 +0900

    [Feature] PRNG(의사 난수 생성기)

---
+++ Algorithms/PRNG (Pseudo Random Number Generator)/prng.c
@@ -0,0 +1,25 @@
+﻿#include "prng.h"
+
+uint64_t PrngSplitMix64(uint64_t x)
+{
+#define PRNG_SPLIT_MIX_64_X 0x9E3779B97F4A7C15
+#define PRNG_SPLIT_MIX_64_A 0xBF58476D1CE4E5B9
+#define PRNG_SPLIT_MIX_64_B 0x94D049BB133111EB
+
+    x += PRNG_SPLIT_MIX_64_X;
+    x = (x ^ (x >> 30)) * PRNG_SPLIT_MIX_64_A;
+    x = (x ^ (x >> 27)) * PRNG_SPLIT_MIX_64_B;
+
+    return x ^ (x >> 31);
+}
+
+float SeedToFloat01(uint64_t seed)
+{
+    seed &= 0x007FFFFF;
+    seed |= 0x3F800000;
+
+    uint32_t buffer = (uint32_t)seed;
+    float* ptrValue = (float*)(&buffer);
+
+    return *ptrValue - 1.0f;
+}
---
+++ Algorithms/PRNG (Pseudo Random Number Generator)/prng.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <stdint.h>
+
+uint64_t PrngSplitMix64(uint64_t x);
+
+float SeedToFloat01(uint64_t seed);
